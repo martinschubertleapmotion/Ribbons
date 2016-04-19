807b0ba6392caacf9189e2dd290a90853e7294db
Martin Schubert <mschubert@leapmotion.com>
2016-04-19 00:41:24 -0700

    Added PlayerHead, fixing hand collision result.

---
+++ Ribbons_Project/Ribbons/Assets/MyScripts/CopyLeftHand.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopyLeftHand : MonoBehaviour {
+
+		public GameObject ObjectToCopy; // This you assign in the inspector
+		public Transform NewParent;  
+
+		void Start( )
+		{
+		Instantiate(ObjectToCopy, transform.position, transform.rotation);
+		ObjectToCopy.gameObject.name = "FrozenLeftHand";
+		ObjectToCopy.gameObject.SetActive(true);
+		ObjectToCopy.transform.SetParent (NewParent);
+		}
+	}
---
+++ Ribbons_Project/Ribbons/Assets/MyScripts/CopyRightHand.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopyRightHand : MonoBehaviour {
+
+		public GameObject ObjectToCopy; // This you assign in the inspector
+		public Transform NewParent; 
+
+		public void Start( )
+		{
+		Instantiate(ObjectToCopy, transform.position, transform.rotation);
+		ObjectToCopy.gameObject.name = "FrozenRightHand";
+		ObjectToCopy.gameObject.SetActive(true);
+		ObjectToCopy.transform.SetParent (NewParent);
+		}
+	}
